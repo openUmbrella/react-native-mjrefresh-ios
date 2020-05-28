3776a84d039eadbc82d07ef0976ba36c7ffe1cfb
lijie <lijie@wshifu.com>
2020-05-28 14:04:15 +0800

    更新example

--- example/src/router/mainStackRouter.js
+++ example/src/router/mainStackRouter.js
@@ -1,30 +1,30 @@
-import {
-    createStackNavigator
-} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
 import HomeScreen from '../screen/homeScreen';
 import ListDemoScreen from '../screen/listDemoScreen';
 import WebViewScreen from '../screen/webViewScreen';
+import TipsScreen from '../screen/tipsScreen';
 
-
-export default stackContainer = createStackNavigator({
-    Home: {
-        screen: HomeScreen,
-        navigationOptions: {
-            title: '演示',
-            // headerStyle: {
-            //     backgroundColor: '#10f9fa'
-            // }
-            
-        }
+export default createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      title: '演示',
+      // headerStyle: {
+      //     backgroundColor: '#10f9fa'
+      // }
     },
-    ListDemo: {
-        screen: ListDemoScreen
+  },
+  ListDemo: {
+    screen: ListDemoScreen,
+  },
+  WebView: {
+    screen: WebViewScreen,
+  },
+  Tips: {
+    screen: TipsScreen,
+    navigationOptions: {
+      title: '欢迎使用Tips组件',
     },
-    WebView: {
-        screen: WebViewScreen
-    }
+  },
 });
-
-
-
